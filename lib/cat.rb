b1599a293b2fd61298f5ea8a2a7ccc67a8ce6322
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-07-17 22:28:24 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -5,12 +5,13 @@ class Cat
   attr_reader :name
   attr_writer :mood
   
-  @@all =[]
+  @@all = []
   
   def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = "nervous"
+    @@all << self
   end
   
   def mood
